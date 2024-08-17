445b0b9f80a6657e5dd0f3c8ed2732fe1d19e69d
Artem Velychko <avelychko@icloud.com>
2024-08-17 18:24:52 -0400

    Changes made

--- components/materials/fields/index.tsx
+++ components/materials/fields/index.tsx
@@ -1,5 +1,5 @@
 export { InputField } from './input-field';
-export { SelectField } from './profiles-field';
+export { ProfilesField } from './profiles-field';
 export { FragrancePyramidField } from './fragrance-pyramid-field';
 export { IFRALimitField } from './ifra-limit-field';
 export { DilutionsField } from './dilutions-field';
